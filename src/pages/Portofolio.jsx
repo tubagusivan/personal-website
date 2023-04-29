44dc5bd059c2786b8c467b280e4e276a8b93d772
tubagusivan <ivanbudiman.tb@gmail.com>
2023-04-29 19:41:41 +0700

    feat: add card on portofolio page & add link for github, linkedIn, instagram, etc.

--- src/pages/Portofolio.jsx
+++ src/pages/Portofolio.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../store/action/actionCreator";
+import hr from '../assets/curve-hr.svg'
 
 function Portofolio() {
     const dispatch = useDispatch()
@@ -13,11 +14,11 @@ function Portofolio() {
     return (
         <section className="mt-12 mx-auto px-4 max-w-screen-xl md:px-8">
             <div className="text-center bg">
-                <h1 className="text-3xl text-gray-800 font-semibold">
-                    Blog
+                <h1 className="mt-3 text-3xl text-gray-800 font-semibold">
+                    Portofolio
                 </h1>
                 <p className="mt-3 text-gray-500">
-                    Blogs that are loved by the community. Updated every hour.
+                    Below are my list project.
                 </p>
             </div>
             <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
@@ -32,7 +33,7 @@ function Portofolio() {
                                     </div>
                                     <div className="ml-3">
                                         <span className="block text-gray-900">{items.authorName}</span>
-                                        <span className="block text-gray-400 text-sm">{items.date}</span>
+                                        <span className="block text-gray-400 text-sm">Project Date: {items.date}</span>
                                     </div>
                                 </div>
                                 <div className="pt-3 ml-4 mr-2 mb-3">
@@ -45,6 +46,9 @@ function Portofolio() {
                         </article>
                     ))
                 }
+            </div>
+            <div id="about" className="flex md:h-20 gap-5 items-center justify-center text-white relative">
+                <img src={hr} className="w-full md:h-2 absolute bottom-0" alt="hr" />
             </div>
         </section>
     )
